f8c6ec33862ea36bac1efadfd8d5e9cb27d2f697
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-10-08 14:30:03 +0500

    First program in c# it is a basic some lines of code

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,6 +11,13 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hey I am Human");
+            Console.WriteLine("Who are you What are you doing on Earth?");
+            Console.WriteLine(3 + 23);
+            //if we want to add strings at the same line
+            Console.Write("I am an alien");
+            Console.Write(" I came from an another planet to attack you planet");
+            Console.ReadLine();
         }
     }
 }
